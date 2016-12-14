c0b133ea2e1391a9f3f25fc7e5819727383aef94
nandhana <nandhanasimon4@gmail.com>
2016-12-14 12:33:23 +0530

    route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,36 @@ Route::get('/', function () {
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
+
+Route::auth();
+
+// city part
+
+Route::get('/home', 'HomeController@index');
+
+
+Route::get('/createcity','CityController@create');
+
+Route::post('/storecity','CityController@store');
+
+Route::get('/showcity','CityController@show');
+
+Route::get('/editcity/{num}','CityController@edit');
+
+Route::post('/updatecity/{num}','CityController@update');
+
+Route::get('/deletecity/{num}','CityController@delete');
+
+//venue part
+
+Route::get('/createvenue','VenueController@create');
+
+Route::post('/storevenue','VenueController@store');
+
+Route::get('/showvenue','VenueController@show');
+
+Route::get('/editvenue/{num}','VenueController@edit');
+
+Route::post('/updatevenue/{num}','VenueController@update');
+
+Route::get('/deletevenue/{num}','VenueController@delete');
